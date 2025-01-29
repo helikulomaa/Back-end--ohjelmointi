88e464f1c3c866c9df8d5f44fb9044d9b401671b
Heli Kulomaa <heli.kulomaa@gmail.com>
2025-01-29 18:56:55 +0200

    Create an entity from your Book class

--- bookstore/src/main/java/k25/bookstore/domain/Book.java
+++ bookstore/src/main/java/k25/bookstore/domain/Book.java
@@ -1,16 +1,30 @@
 package k25.bookstore.domain;
 
-public class Book {
-    String title;
-    String author;
-    int publicationYear;
-    String isbn;
-    double price;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    @Override
-    public String toString() {
-        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
-                + ", price=" + price + "]";
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int publicationYear, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
     }
 
     public String getTitle() {
@@ -53,11 +67,9 @@ public class Book {
         this.price = price;
     }
 
-    public Book(String title, String author, int publicationYear, String isbn, double price) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-        this.isbn = isbn;
-        this.price = price;
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
     }
 }
