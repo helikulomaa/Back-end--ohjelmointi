7f742a79ed0d0cfa8b9805a5a4f162e2eb28955c
Heli Kulomaa <heli.kulomaa@gmail.com>
2025-01-29 19:04:45 +0200

    Add command line runner to insert few example data to the database

--- bookstore/src/main/java/k25/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/k25/bookstore/BookstoreApplication.java
@@ -1,13 +1,28 @@
 package k25.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import k25.bookstore.domain.Book;
+import k25.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			// save a couple of books
+			repository.save(new Book("1984", "George Orwell", 1949, "123-456-789", 10.0));
+			repository.save(new Book("Brave New World", "Aldous Huxley", 1932, "987-654-321", 12.0));
+			repository.save(new Book("Fahrenheit 451", "Ray Bradbury", 1953, "321-654-987", 11.0));
+		};
 	}
 
 }
